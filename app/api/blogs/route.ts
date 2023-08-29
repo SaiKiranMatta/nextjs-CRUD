cdbf1a9e5ccf07c39aa2887c3ab31f195dfb07da
SaiKiranMatta <saikirangoud.matta@gmail.com>
2023-08-29 16:31:25 +0530

    first commit

---
+++ app/api/blogs/route.ts
@@ -0,0 +1,25 @@
+import { getPosts } from "@/lib/data";
+import { NextResponse } from "next/server";
+
+export const GET = async (req: Request, res: Response) => {
+  try {
+    const posts = getPosts();
+    return NextResponse.json({ message: "OK", posts }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }.err, {
+      status: 500,
+    });
+  }
+};
+
+export const POST = async (req: Request, res: Response) => {
+  const { title, desc } = await req.json();
+  try {
+    const posts = { title, desc, date: new Date(), id: Date.now().toString };
+    return NextResponse.json({ message: "OK", posts }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }.err, {
+      status: 500,
+    });
+  }
+};
